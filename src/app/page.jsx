0292120a61e72b915b7e57d139b2bc8f409b4462
ramseySsekandi <moshehravens@gmail.com>
2025-01-15 18:33:47 +0300

    navbar and homepage done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,32 @@
-const Homepage = () => {
-  return <div>Hello World</div>;
-};
+import Image from 'next/image'
+import React from 'react'
 
-export default Homepage;
+const Homepage = () => {
+  return (
+    <div className='h-full flex flex-col px-4 sm:px-8 md:px-12 lg:flex-row lg:px-20 xl:px-48'>
+      {/* IMAGE CONTAINER */}
+      <div className="relative h-1/2 lg:h-full lg:w-1/2">
+        <Image src='/hero.png' alt='' fill className='object-contain'/>
+      </div>
+
+      {/* TEXT CONTAINER */}
+      <div className=" h-1/2 flex flex-col gap-8 items-center justify-center lg:h-full lg:w-1/2">
+        {/* TITLE */}
+        <h1 className='text-4xl font-bold md:text-5xl'>Creating Digital Experiences, Designing Tomorrow.</h1>
+        {/* DESC */}
+        <p className='md:text-xl'>Welcome to my digital canvas, where innovation and creativity
+           converge. With a keen eye for aesthetics and a mastery of code, my
+           portifolio showcases a diverse collection of projects that reflect my
+           commitment to excellence
+           </p>
+        {/* BUTTONS */}
+        <div className="w-full flex gap-4">
+          <button className='p-4 rounded-lg ring ring-black bg-black text-white'>View My Works</button>
+          <button className='p-4 rounded-lg ring ring-black'>Contact Me</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Homepage
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React, { useState } from 'react'
 import NavLink from './NavLink'
+import { motion } from "motion/react"
 
 const Navbar = () => {
   const links = [
@@ -12,8 +13,60 @@ const Navbar = () => {
     {url: "/contact", titles: "Contact"}
   ]
   const [open, setOpen] = useState(false)
+
+  const topVariants = {
+    opened:{
+      rotate:45,
+      backgroundColor:"rgb(255, 255, 255)",
+    },
+    closed:{
+      rotate:0
+    }
+  }
+  const centerVariants = {
+    opened:{
+      opacity:0
+    },
+    closed:{
+      opacity:1
+    }
+  }
+  const bottomVariants = {
+    opened:{
+      rotate:-45,
+      backgroundColor:"rgb(255, 255, 255)",
+    },
+    closed:{
+      rotate:0,
+    }
+  }
+
+  const listVariants = {
+    closed: {
+      x:'100vw'
+    },
+    opened: {
+      x:0,
+      transition: {
+        when:'beforeChildren',
+        staggerChildren:0.2,
+      }
+    }
+  }
+
+  const listItemVariants = {
+    closed: {
+      x:-10,
+      opacity:0,
+    },
+    opened: {
+      x:0,
+      opacity:1,
+    }
+  }
+  
   return (
-    <div className='h-full text-xl flex items-center justify-between px-4 sm:px-8 md:12 lg:20 xl:px-48'>
+    <div className='h-full text-xl flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
       
       {/* LINKS */}
       <div className="hidden md:flex gap-4 w-1/3">
@@ -34,9 +87,6 @@ const Navbar = () => {
 
       {/* SOCIAL */}
       <div className="hidden md:flex gap-4 w-1/3">
-        {/* <Link href='/'>
-          <Image src='/github.png' alt='' width={24} height={24} />
-        </Link> */}
         <Link href='/'>
           <Image src='/github.png' alt='' width={24} height={24} />
         </Link>
@@ -61,19 +111,21 @@ const Navbar = () => {
       <div className="md:hidden">
         {/* MENU BUTTON */}
         <button className='w-10 h-8 flex flex-col justify-between z-50 relative' onClick={()=>setOpen((prev) => !prev)}>
-          <div className="w-10 h-1 bg-white rounded"></div>
-          <div className="w-10 h-1 bg-white rounded"></div>
-          <div className="w-10 h-1 bg-white rounded"></div>
+          <motion.div variants={topVariants} animate={open ? 'opened':'closed'} className="w-10 h-1 bg-black rounded origin-left" ></motion.div>
+          <motion.div variants={centerVariants} animate={open ? 'opened':'closed'} className="w-10 h-1 bg-black rounded"></motion.div>
+          <motion.div variants={bottomVariants} animate={open ? 'opened':'closed'} className="w-10 h-1 bg-black rounded origin-left"></motion.div>
         </button>
         {/* MENU LIST */}
         {open && (
-          <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col justify-center items-center gap-8 text-4xl">
+          <motion.div variants={listVariants} initial='closed' animate='opened' className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col justify-center items-center gap-8 text-4xl z-40">
           {
             links.map((link) => (
-              <Link href={link.url} key={link.titles}>{link.titles}</Link>
+              <motion.div variants={listItemVariants}>
+                <Link href={link.url} key={link.titles}>{link.titles}</Link>
+              </motion.div>
             ))
           }
-        </div>
+          </motion.div>
         )}
       </div>
     </div>
