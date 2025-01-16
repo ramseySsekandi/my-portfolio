600c55feb08aa646fbed8409e2a3f02f275fee9e
ramseySsekandi <moshehravens@gmail.com>
2025-01-16 10:20:44 +0300

    responsive more better but close btn on responsive menu pending

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,14 +5,14 @@ const Homepage = () => {
   return (
     <div className='pb-4 h-full flex gap-1 flex-col px-4 sm:px-8 md:px-12 lg:flex-row lg:px-20 xl:px-48'>
       {/* IMAGE CONTAINER */}
-      <div className="relative h-1/2 lg:h-full lg:w-1/2">
+      <div className="relative h-2/5 lg:h-full lg:w-1/2">
         <Image src='/hero.png' alt='' fill className='object-contain'/>
       </div>
 
       {/* TEXT CONTAINER */}
-      <div className=" h-1/2 flex flex-col gap-2 lg:gap-8 items-center justify-center lg:h-full lg:w-1/2">
+      <div className=" h-3/5 flex flex-col gap-2 lg:gap-8 items-center justify-center lg:h-full lg:w-1/2">
         {/* TITLE */}
-        <h1 className='text-xl sm:text-3xl font-bold md:text-4xl'>Creating Digital Experiences, Designing Tomorrow.</h1>
+        <h1 className='text-2xl sm:text-3xl font-bold md:text-4xl'>Creating Digital Experiences, Designing Tomorrow.</h1>
         {/* DESC */}
         <p className='text-sm sm:text-lg lg:text-xl'>Welcome to my digital canvas, where innovation and creativity
            converge. With a keen eye for aesthetics and a mastery of code, my
